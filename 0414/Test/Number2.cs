9e3e05ada18d2cf57c7f6158cf064dbd92b159c8
영준 배 <byj6614@naver.com>
2023-04-14 20:04:31 +0900

    <feat> : 문자에서 문자열 갯수 구하기

---
+++ 0414/Test/Number2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0414.Test
+{
+    internal class Number2
+    {
+        public static void Test2()
+        {
+            Console.Write("문자열 입력 : ");
+            string a = Console.ReadLine();
+            Console.Write("문자열 갯수 : ");
+            char c = ' ';
+            int b = a.Count(f => f == c) + 1;
+            Console.WriteLine(b);
+        }
+    }
+}
