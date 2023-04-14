8d19d96239e02a82cdce2ffb036e147bd0a809b2
영준 배 <byj6614@naver.com>
2023-04-14 20:01:05 +0900

    <feat> : 문장을 입력하고 단어에 시작위치 찾기

---
+++ 0414/Test/Nunber1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0414.Test
+{
+    internal class Nunber1
+    {
+    }
+}
