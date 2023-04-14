c5bf88ea5e6f4949e3923518721174e720c79d10
영준 배 <byj6614@naver.com>
2023-04-14 20:46:36 +0900

    feat : UpDown 조건
    UpDown이 되는 조건을 작성

---
+++ UpDown/UpDownClass/UpDown.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpDown.UpDownClass
+{
+    internal  class UpDown
+    {
+        public int updown(int a, int b)
+        {
+            if (a < b)
+            {
+                return -1;
+            }
+            else if (a > b)
+            {
+                return 1;
+            }
+            else
+                return 0;
+        }
+    }
+}
