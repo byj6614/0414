88558136088a03f956af00a2eaca068cb932cd23
영준 배 <byj6614@naver.com>
2023-04-14 21:02:05 +0900

    feat : 플레이 시작
    UpDown의 플레이를 시작하는 함수

---
+++ UpDown/UpDownClass/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpDown.UpDownClass
+{
+    internal class Player
+    {
+        public void player() 
+        {
+            UpDown upDown= new UpDown();
+            int num = NumRand.Num();
+            int me;
+            string retry;
+            r:
+            Console.Clear();
+            for (int i = 0; i <= 4; i++)
+            {
+
+                if (i == 4)
+                {
+                    Console.WriteLine("You Lose");
+                    Console.WriteLine("Re Try?");
+                    Console.WriteLine("YES:R   NO:N");
+                    retry= Console.ReadLine();
+                    if(retry=="r")
+                    {
+                        goto r;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    me = int.Parse(Console.ReadLine());
+                    if (upDown.updown(me, num) == 0)
+                    {
+                        Console.WriteLine("Clear");
+                        break;
+                    }
+                    else if (upDown.updown(me, num) == 1)
+                    {
+                        Console.WriteLine("Down");
+                    }
+                    else if (upDown.updown(me, num) == -1)
+                    {
+                        Console.WriteLine("Up");
+                    }
+                }
+
+            }
+        }
+    }
+}
