b1722002288058eaf39cfabaf209c1a43593d5fb
영준 배 <byj6614@naver.com>
2023-04-14 20:24:57 +0900

    feat : 단어 위치 찾기

---
+++ 0414/Test/Number1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0414.Test
+{
+    internal class Number1
+    {
+        public static void Test1()
+        {
+            Console.Write("문장을 입력 : ");
+            string str = Console.ReadLine();
+            Console.Write("단어를 입력 : ");
+            string str2 = Console.ReadLine();
+            int num = str.IndexOf(str2);
+            Console.Write("단어가 들어간 공간 : {0}", num);
+        }
+    }
+}
