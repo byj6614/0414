5087e877febd9ec1abccd904a592471597f75bdc
영준 배 <byj6614@naver.com>
2023-04-14 21:13:56 +0900

    feat : 슬라이드 게임 input 설정
    키를 입력해주는 함수 생성

---
+++ SlideGame/SlideGame/Sinput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlideGame.SlideGame
+{
+    internal class Sinput
+    {
+        public static int sinput()
+        {
+            ConsoleKeyInfo info= Console.ReadKey();
+            int a;
+            switch(info.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    a = 1;
+                    break;
+                    case ConsoleKey.DownArrow:
+                    a = 2;
+                    break;
+                    case ConsoleKey.LeftArrow:
+                    a = 3;
+                    break;
+                    case ConsoleKey.RightArrow:
+                    a = 4;
+                    break;
+                    default:
+                    a = 0;
+                    break;
+            }
+            return a;
+        }
+    }
+}
