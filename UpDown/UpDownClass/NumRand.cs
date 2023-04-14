6c5a3eee1a03cf10085d97c3c9c5cc2339dc1643
영준 배 <byj6614@naver.com>
2023-04-14 20:44:34 +0900

    feat : UpDown 랜덤숫자
    랜덤한 숫자를 만드는 클래스

---
+++ UpDown/UpDownClass/NumRand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpDown.UpDownClass
+{
+    internal static class NumRand
+    {
+        public static int Num()
+        {
+            Random random = new Random();
+            int randnum = random.Next(0, 999);
+            return randnum;
+        }
+       
+        
+    }
+}
