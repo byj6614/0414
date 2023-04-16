d186fb03da1f802916b7ff4eaeb569aeb0cd823a
영준 배 <byj6614@naver.com>
2023-04-16 19:52:27 +0900

    feat : 화면을 렌더링

---
+++ SlideGame/SlideGame/SRender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlideGame.SlideGame
+{
+    internal class SRender
+    {
+        public void srender(int[,] a)
+        {
+            Console.Clear();
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int y=0;y<a.GetLength(1);y++)
+                {
+                    Console.Write(a[i,y]+"    ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
