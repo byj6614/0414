9724ec8c58b5555c629968849ddbe14115502875
영준 배 <byj6614@naver.com>
2023-04-16 18:34:22 +0900

    feat : 플레이어 기초공간
    플레이어의 기초공간을 저장하기 위한곳

---
+++ SlideGame/SlideGame/Playerspace.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlideGame.SlideGame
+{
+    internal class Playerspace
+    {
+        public int x;
+        public int y;
+        public Playerspace(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
